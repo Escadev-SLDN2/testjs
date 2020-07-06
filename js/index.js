637486fec8e6a084f3256d24dc78d1ec202edf33
Nicolas Vauché <hello@nicolasvauche.net>
2020-07-06 10:49:42 +0200

    bad code format

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-const fs = require('fs')
+const fs = require('fs');
 
-const http = require('http')
+const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Salut à toi')
+    res.end('Salut à toi');
 })
 
 server.listen(8000, '127.0.0.1', () => {
-  console.log('Server running on http://localhost:8000')
+    console.log('Server running on http://localhost:8000');
 })
