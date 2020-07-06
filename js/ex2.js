9fb22ed7b2ae3c9d30d283cd90a649fc7951be82
Papibadiste <labussieremailpro@gmail.com>
2020-07-06 12:57:41 +0200

    oui

---
+++ js/ex2.js
@@ -0,0 +1,14 @@
+function getGreen() {
+
+        document.getElementById('colored').setAttribute('style', 'background-color: green')
+
+  }
+
+  function getOrange() {
+
+    document.getElementById('colored').setAttribute('style', 'background-color: orange')
+
+}
+function getRed() {
+    document.getElementById('colored').setAttribute('style', 'background-color: red')
+}
--- js/index.js
+++ js/index.js
@@ -1,6 +1,1 @@
-let Reponse = '';
-while (Reponse.length == 0) {
-  Reponse = window.prompt('Quelle est la réponse ?', '');
-}
-document.getElementById("console").innerHTML = Reponse;
 
