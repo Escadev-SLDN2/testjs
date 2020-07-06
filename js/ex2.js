f982bd2f5777804532485e8f117b0e5f261458f6
Montaudon Sam <sam.montaudon@gmail.com>
2020-07-06 12:27:21 +0200

    oups

--- js/ex2.js
+++ js/ex2.js
@@ -1,9 +1,9 @@
 document.getElementById('orange').addEventListener('click', function () {
-    alert('Orange!')
-  })
-  document.getElementById('vert').addEventListener('click', function () {
-      alert('Vert!')
-    })
-    document.getElementById('rouge').addEventListener('click', function () {
-      alert('Rouge!')
-    })
+  alert('Orange!')
+})
+document.getElementById('vert').addEventListener('click', function () {
+  alert('Vert!')
+})
+document.getElementById('rouge').addEventListener('click', function () {
+  alert('Rouge!')
+})
