b7377da965fd0e9a0694ef4b2a84892d12ffcaa6
Hime Hime <jlateve@gmail.com>
2020-07-06 12:38:36 +0200

    exercice event listener

--- js/index.js
+++ js/index.js
@@ -5,3 +5,27 @@ console.log(helloStr)
 if (typeof document !== 'undefined') {
   document.getElementById('console').innerHTML = helloStr
 }
+
+
+
+
+
+
+const btnRouge = document.getElementById('rouge');
+
+btnRouge.addEventListener('click', function (event) {
+  alert('Tu veux du rouge!');
+});
+
+const btnVert = document.getElementById('vert');
+
+btnVert.addEventListener('click', function (event) {
+  alert('Tu veux du vert!');
+});
+
+const btnOrange = document.getElementById('orange');
+
+btnOrange.addEventListener('click', function (event) {
+  alert('Tu veux du orange!');
+});
+
