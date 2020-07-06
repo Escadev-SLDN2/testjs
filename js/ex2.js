0c4f520364ed8d6b293c01d903985e73691baeb6
Papibadiste <labussieremailpro@gmail.com>
2020-07-06 14:19:20 +0200

    oui

--- js/ex2.js
+++ js/ex2.js
@@ -2,16 +2,20 @@
 
 
 function getColor() {
+    
     const btnRouge = document.getElementById('rouge');
     const btnVert = document.getElementById('vert');
     const btnOrange = document.getElementById('orange');
-btnRouge.addEventListener('click', function (event) {
-document.getElementById('colored').setAttribute('style', 'background-color: red')
-});
-btnVert.addEventListener('click', function (event) {
-document.getElementById('colored').setAttribute('style', 'background-color: green')
-});
-btnOrange.addEventListener('click', function (event) {
-document.getElementById('colored').setAttribute('style', 'background-color: orange')
-});
+
+
+    btnRouge.addEventListener('click', function (event) {
+        document.getElementById('colored').setAttribute('style', 'background-color: red')
+    });
+
+    btnVert.addEventListener('click', function (event) {
+        document.getElementById('colored').setAttribute('style', 'background-color: green')
+    });
+    btnOrange.addEventListener('click', function (event) {
+        document.getElementById('colored').setAttribute('style', 'background-color: orange')
+    });
 }
